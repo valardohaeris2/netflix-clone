bc4bd476e6e1cbbdfc5609d9f8b8bb48e9e2d8fd
valardohaeris2 <aconner410@proton.me>
2025-04-21 18:35:27 -0400

    Update java requests

--- src/requests.js
+++ src/requests.js
@@ -1,7 +1,7 @@
 const API_KEY = "183b5059754f3f1442e9dfbd6f022e96";
 
 const requests = {
-  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`
   fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
   fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
   fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
